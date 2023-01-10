6d60ad337ca6390c4688e0899b26134f67646c49
Sergiy B <bisecom@gmail.com>
2023-01-10 09:49:03 +0100

    Initial commit

---
+++ src/main/java/com/webvisitors/WebVisitorsApplication.java
@@ -0,0 +1,13 @@
+package com.webvisitors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebVisitorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebVisitorsApplication.class, args);
+	}
+
+}
