0c8eafed81f7f8cb769aac902c6eeb5914ae1fb9
Sergiy B <bisecom@gmail.com>
2023-01-10 09:48:28 +0100

    Initial commit

---
+++ src/test/java/com/webvisitors/WebVisitorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webvisitors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebVisitorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
